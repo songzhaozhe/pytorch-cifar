098d3a6dc8f5fbe3a293c9dd88edac958374c561
songzhaozhe <songzhaozhe@126.com>
2018-03-20 16:38:32 +0800

    bug fixed

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import argparse
 
 from models import *
 from torch.autograd import Variable
-from torch.optim.lr_scheduler import LambdaLR
+from torch.optim.lr_scheduler import LambdaLR, MultiStepLR
 from utils import init_params
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
@@ -27,9 +27,11 @@ parser.add_argument('--resume', '-r', action='store_true', help='resume from che
 parser.add_argument('--model', '-m', help='resume from model file name')
 args = parser.parse_args()
 
-max_epoch = 180
+max_epoch = 164
 
 def schedule(epoch):
+    #if (epoch < 500):
+    #    return 0.01
     if (epoch <= 0.5*max_epoch):
         return 0.1
     elif (epoch <= 0.75*max_epoch):
@@ -43,27 +45,30 @@ best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
 
+mean = [x/255 for x in [125.3, 123.0, 113.9]]
+
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (1,1,1)),
+    transforms.Normalize(mean, [1,1,1]),
     #transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
 transform_test = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (1,1,1)),
+    transforms.Normalize(mean, [1,1,1]
+        ),
     #transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=10)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=5, pin_memory=True)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=10)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=5, pin_memory=True)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
@@ -84,8 +89,8 @@ if args.resume:
 else:
     print('==> Building model..')
     # net = VGG('VGG19')
+    #net = ResNet32()
     net = ResNet20()
-    #net = ResNet110()
     # net = PreActResNet18()
     # net = GoogLeNet()
     # net = DenseNet121()
@@ -100,19 +105,17 @@ else:
 if use_cuda:
     net.cuda()
     net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
-    cudnn.benchmark = True
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=1e-4, nesterov=True)
-scheduler = LambdaLR(optimizer, schedule)
-
+#scheduler = LambdaLR(optimizer, schedule)
+scheduler = MultiStepLR(optimizer, milestones=[int(max_epoch*0.5), int(max_epoch*0.75)], gamma=0.1)
 # Training
 def train(epoch):
     print('\nEpoch: %d' % epoch)
     net.train()
     train_loss = 0
-    correct = 0
-    total = 0
+    acc = 0
     t = trange(total_batch)
     dataiter = iter(trainloader)
 
@@ -121,18 +124,19 @@ def train(epoch):
         t.set_description('Batch num %i %i' % (batch_idx, total_batch))
         if use_cuda:
             inputs, targets = inputs.cuda(), targets.cuda()
-        optimizer.zero_grad()
         inputs, targets = Variable(inputs), Variable(targets)
         outputs = net(inputs)
         loss = criterion(outputs, targets)
+        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        train_loss += loss.data[0]
+        train_loss = train_loss*0.2 + loss.data[0] * 0.8
         _, predicted = torch.max(outputs.data, 1)
-        total += targets.size(0)
-        correct += predicted.eq(targets.data).cpu().sum()
-        t.set_postfix(loss = '%.4f' % (train_loss/(batch_idx+1)), Acc = '%.4f' % (100.*correct/total))
+        total = targets.size(0)
+        correct = predicted.eq(targets.data).cpu().sum()
+        acc = acc * 0.2 + 0.8 * 100. * correct / total
+        t.set_postfix(loss = '%.4f' % train_loss, Acc = '%.4f' % acc)
 
 def test(epoch):
     global best_acc
@@ -147,7 +151,7 @@ def test(epoch):
         t.set_description('Batch num %i %i' % (batch_idx, total_batch))
         if use_cuda:
             inputs, targets = inputs.cuda(), targets.cuda()
-        inputs, targets = Variable(inputs, volatile=True), Variable(targets)
+        inputs, targets = Variable(inputs, volatile=True), Variable(targets, volatile=True)
         outputs = net(inputs)
         loss = criterion(outputs, targets)
 
@@ -183,8 +187,14 @@ def test(epoch):
         torch.save(state, './checkpoint/last_ckpt%.3f.t7'%acc)
         print('Best accuracy is %.3f' % best_acc)
 
+def adjust_learning_rate(optimizer, epoch):
+  """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+  lr = schedule(epoch)
+  for param_group in optimizer.param_groups:
+    param_group['lr'] = lr
 
 for epoch in range(start_epoch, max_epoch+1):
+    #adjust_learning_rate(optimizer, epoch)
     scheduler.step()
     train(epoch)
     test(epoch)
--- models/resnet.py
+++ models/resnet.py
@@ -10,11 +10,21 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
-
+import math
 from torch.autograd import Variable
 
 EPS = 1e-9
 MO = 0.05
+class DownsampleA(nn.Module):  
+
+  def __init__(self, nIn, nOut, stride):
+    super(DownsampleA, self).__init__() 
+    assert stride == 2    
+    self.avg = nn.AvgPool2d(kernel_size=1, stride=stride)   
+
+  def forward(self, x):   
+    x = self.avg(x)  
+    return torch.cat((x, x.mul(0)), 1)  
 
 class BasicBlock(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
@@ -23,12 +33,13 @@ class BasicBlock(nn.Module):
         self.bn1 = nn.BatchNorm2d(planes, eps = EPS, momentum = MO)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes, eps = EPS, momentum = MO)
-
+        self.avg = nn.AvgPool2d(kernel_size=1, stride=stride)
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes, eps = EPS, momentum = MO)
+                #nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=False),
+                #nn.BatchNorm2d(planes, eps = EPS, momentum = MO)
+                DownsampleA(in_planes, planes, stride)
             )
 
     def forward(self, x):
@@ -49,16 +60,27 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
         self.linear = nn.Linear(64, num_classes)      
-        init.xavier_uniform(self.linear.weight)
+        #init.kaiming_normal(self.linear.weight)
 
-        for key in self.state_dict():
-            if key.split('.')[-1] == 'weight':
-                if 'conv' in key:
-                    init.kaiming_normal(self.state_dict()[key], mode='fan_out')
-                if 'bn' in key:
-                    self.state_dict()[key][...] = 1
-            elif key.split('.')[-1] == 'bias':
-                self.state_dict()[key][...] = 0
+        for m in self.modules():
+          if isinstance(m, nn.Conv2d):
+            n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            m.weight.data.normal_(0, math.sqrt(2. / n))
+            #m.bias.data.zero_()
+          elif isinstance(m, nn.BatchNorm2d):
+            m.weight.data.fill_(1)
+            m.bias.data.zero_()
+          elif isinstance(m, nn.Linear):
+            init.kaiming_normal(m.weight)
+            m.bias.data.zero_()
+        # for key in self.state_dict():
+        #     if key.split('.')[-1] == 'weight':
+        #         if 'conv' in key:
+        #             init.kaiming_normal(self.state_dict()[key])
+        #         if 'bn' in key:
+        #             self.state_dict()[key][...] = 1
+        #     elif key.split('.')[-1] == 'bias':
+        #         self.state_dict()[key][...] = 0
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -74,7 +96,6 @@ class ResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = F.avg_pool2d(out, 8)
-        print(out.size(0))
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
@@ -82,6 +103,8 @@ class ResNet(nn.Module):
 def ResNet20():
     return ResNet(BasicBlock, [3,3,3])
 
+def ResNet32():
+    return ResNet(BasicBlock, [5,5,5])
 def ResNet110():
     return ResNet(BasicBlock, [18,18,18])
 
